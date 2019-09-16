dbe04378e4b566e6824ff7d7e62570dc6be5eefd
hirao <hirao@combine.co.jp>
2019-09-16 17:01:10 +0900

    ダメージをランダムにする。

--- pages/battle.vue
+++ pages/battle.vue
@@ -36,14 +36,18 @@ export default class Battle extends Vue {
   battle_state = 0;
 
   attack() {
-    this.enemy_hp--;
-    this.messages.push("1のダメージを与えた");
+    const max = 3,
+      min = 1;
+    const enemy_damage = Math.floor(Math.random() * (max + 1 - min)) + min;
+    this.enemy_hp -= enemy_damage;
+    this.messages.push(`${enemy_damage}のダメージを与えた`);
     if (this.enemy_hp <= 0) {
       this.messages.push("敵を倒した");
       this.battle_state = 1;
     } else {
-      this.hp--;
-      this.messages.push("1のダメージを受けた");
+      const damage = Math.floor(Math.random() * (max - min)) + min;
+      this.hp -= damage;
+      this.messages.push(`${damage}のダメージを受けた`);
       if (this.hp <= 0) {
         this.messages.push("負けた");
         this.battle_state = -1;
